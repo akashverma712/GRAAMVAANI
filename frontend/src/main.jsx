c2a5f07c267fb9ae1a78d89918a79d6abb7930e5
akashverma712 <aakashverma7122004@gmail.com>
2025-08-05 02:20:38 +0530

    converted it to a PWA

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,17 +5,39 @@ import './index.css';
 
 import { ClerkProvider } from '@clerk/clerk-react';
 import { BrowserRouter } from 'react-router-dom';
+import { registerSW } from 'virtual:pwa-register';
+
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New update available. Reload now?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    alert("You are now offline-ready");
+  }
+});
+
+
+window.addEventListener('offline', () => {
+  alert('You are offline!');
+});
+
+window.addEventListener('online', () => {
+  alert('You are back online!');
+});
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!clerkPubKey) throw new Error('VITE_CLERK_PUBLISHABLE_KEY is missing');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<ClerkProvider publishableKey={clerkPubKey}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</ClerkProvider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
+  </React.StrictMode>
 );
